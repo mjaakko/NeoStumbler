2aa50fe78832907d184cceaa72fcfa694cd5da6b
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-10-03 18:37:43 +0300

    Create test for filtering Wi-Fi scan results

---
+++ app/src/test/java/xyz/malkki/wifiscannerformls/extensions/ScanResultExtensionsTest.kt
@@ -0,0 +1,36 @@
+package xyz.malkki.wifiscannerformls.extensions
+
+import android.net.wifi.ScanResult
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+import xyz.malkki.wifiscannerformls.scanner.filterForMLS
+
+class ScanResultExtensionsTest {
+    @Test
+    fun `Test filtering scan results`() {
+        val valid = listOf(
+            ScanResult().apply {
+                SSID = "test"
+            },
+            ScanResult().apply {
+                SSID = "hello_123"
+            }
+        )
+        val invalid = listOf(
+            ScanResult().apply {
+                SSID = "my_network_nomap"
+            },
+            ScanResult().apply {
+                SSID = ""
+            }
+        )
+
+        val scanResults = valid + invalid
+
+        val filtered = scanResults.filterForMLS()
+
+        assertEquals(valid, filtered)
+    }
+}
