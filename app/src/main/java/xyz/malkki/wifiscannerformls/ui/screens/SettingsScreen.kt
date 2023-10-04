de7d2afd5ecf770f13486b49bfc09b04e23b6ea4
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-10-04 21:04:28 +0300

    Create Composable for displaying settings

---
+++ app/src/main/java/xyz/malkki/wifiscannerformls/ui/screens/SettingsScreen.kt
@@ -0,0 +1,12 @@
+package xyz.malkki.wifiscannerformls.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import xyz.malkki.wifiscannerformls.ui.composables.AutoScanToggle
+
+@Composable
+fun SettingsScreen() {
+    Column {
+        AutoScanToggle()
+    }
+}
