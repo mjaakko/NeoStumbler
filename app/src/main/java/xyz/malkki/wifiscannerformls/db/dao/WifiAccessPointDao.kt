4239abb6561207570e118c34fe81deec2dffbc0c
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-10-04 22:14:31 +0300

    Add functions for getting counts of distinct devices

--- app/src/main/java/xyz/malkki/wifiscannerformls/db/dao/WifiAccessPointDao.kt
+++ app/src/main/java/xyz/malkki/wifiscannerformls/db/dao/WifiAccessPointDao.kt
@@ -1,11 +1,18 @@
 package xyz.malkki.wifiscannerformls.db.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import xyz.malkki.wifiscannerformls.db.entities.WifiAccessPoint
 
 @Dao
 interface WifiAccessPointDao {
     @Insert
     suspend fun insertAll(vararg wifiAccessPoint: WifiAccessPoint)
+
+    @Query("""
+        SELECT COUNT(DISTINCT macAddress) FROM WifiAccessPoint
+    """)
+    fun countDistinct(): LiveData<Long>
 }
