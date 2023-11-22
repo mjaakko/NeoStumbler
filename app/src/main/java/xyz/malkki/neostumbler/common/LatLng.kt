88d02bd23cb929b362bd963de95bfa20a2f6207f
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-11-22 07:22:24 +0200

    Create function for getting the latest position from the DB

---
+++ app/src/main/java/xyz/malkki/neostumbler/common/LatLng.kt
@@ -0,0 +1,9 @@
+package xyz.malkki.neostumbler.common
+
+/**
+ * Simple value class describing a latitude/longitude pair
+ */
+data class LatLng(
+    val latitude: Double,
+    val longitude: Double
+)
--- app/src/main/java/xyz/malkki/neostumbler/db/dao/PositionDao.kt
+++ app/src/main/java/xyz/malkki/neostumbler/db/dao/PositionDao.kt
@@ -2,10 +2,22 @@ package xyz.malkki.neostumbler.db.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import xyz.malkki.neostumbler.common.LatLng
 import xyz.malkki.neostumbler.db.entities.Position
 
 @Dao
 interface PositionDao {
     @Insert
     suspend fun insert(position: Position)
+
+    /**
+     * Gets coordinates of the latest position as a pair
+     *
+     * @return Pair of coordinates (latitude, longitude)
+     */
+    @Transaction
+    @Query("SELECT p.latitude AS latitude, p.longitude AS longitude FROM Position p JOIN Report r ON p.reportId = r.id ORDER BY r.timestamp DESC LIMIT 1")
+    suspend fun getLatestPosition(): LatLng?
 }
