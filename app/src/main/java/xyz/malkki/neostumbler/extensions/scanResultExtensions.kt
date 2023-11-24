15ff975076d1d526431741b56fa44db430f9947f
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-11-24 18:05:50 +0200

    Trim whitespace from Wi-Fi SSIDs to avoid sending bad data

--- app/src/main/java/xyz/malkki/neostumbler/extensions/scanResultExtensions.kt
+++ app/src/main/java/xyz/malkki/neostumbler/extensions/scanResultExtensions.kt
@@ -3,12 +3,21 @@ package xyz.malkki.neostumbler.extensions
 import android.net.wifi.ScanResult
 import android.os.Build
 
+/**
+ * SSID of the network without quotation marks and surrounding whitespace
+ */
 val ScanResult.ssidString: String?
-    get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        wifiSsid?.toString()
-    } else {
-        SSID
-    }?.replace(Regex("(^\"|\"\$)"), "") //Remove quotation marks from beginning and end
+    get() {
+        val ssid = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            wifiSsid?.toString()
+        } else {
+            SSID
+        }
+
+        return ssid
+            ?.replace(Regex("(^\"|\"\$)"), "") //Remove quotation marks from beginning and end
+            ?.trim() //Remove surrounding whitespace to avoid writing empty values to the DB
+    }
 
 val ScanResult.timestampMillis: Long
     get() = timestamp / 1000
