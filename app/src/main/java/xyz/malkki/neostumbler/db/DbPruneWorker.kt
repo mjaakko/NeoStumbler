0ecc63a494ed59db6653b14491ce2e9486d05c26
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-11-24 18:09:16 +0200

    Add number of deleted reports to the output data of DbPruneWorker

--- app/src/main/java/xyz/malkki/neostumbler/db/DbPruneWorker.kt
+++ app/src/main/java/xyz/malkki/neostumbler/db/DbPruneWorker.kt
@@ -2,6 +2,7 @@ package xyz.malkki.neostumbler.db
 
 import android.content.Context
 import androidx.work.CoroutineWorker
+import androidx.work.Data
 import androidx.work.WorkerParameters
 import timber.log.Timber
 import xyz.malkki.neostumbler.StumblerApplication
@@ -17,7 +18,9 @@ class DbPruneWorker(appContext: Context, params: WorkerParameters) : CoroutineWo
         const val PERIODIC_WORK_NAME = "db_prune_periodic"
         const val ONE_TIME_WORK_NAME = "db_prune_one_time"
 
-        const val MAX_AGE_DAYS = "max_age_days"
+        const val INPUT_MAX_AGE_DAYS = "max_age_days"
+
+        const val OUTPUT_REPORTS_DELETED = "reports_deleted"
     }
 
     override suspend fun doWork(): Result {
@@ -25,7 +28,7 @@ class DbPruneWorker(appContext: Context, params: WorkerParameters) : CoroutineWo
         val reportDao = db.reportDao()
 
         //By default delete reports older than 60 days
-        val maxAgeDays = inputData.getLong(MAX_AGE_DAYS, 60)
+        val maxAgeDays = inputData.getLong(INPUT_MAX_AGE_DAYS, 60)
 
         val minTimestamp = ZonedDateTime.now().minusDays(maxAgeDays).toInstant()
 
@@ -37,6 +40,6 @@ class DbPruneWorker(appContext: Context, params: WorkerParameters) : CoroutineWo
 
         Timber.i("Deleted $deleteCount reports in ${duration.toString(DurationUnit.SECONDS, 1)}")
 
-        return Result.success()
+        return Result.success(Data.Builder().putInt(OUTPUT_REPORTS_DELETED, deleteCount).build())
     }
 }
