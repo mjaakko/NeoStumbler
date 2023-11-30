bc0f76beebb065ef37d0f1c5ea9627913959c6f4
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-11-30 17:44:42 +0200

    Simplify LocationReceiver by only removing geofences when they are no longer needed and updating them in other cases

--- app/src/main/java/xyz/malkki/neostumbler/scanner/autoscan/LocationReceiver.kt
+++ app/src/main/java/xyz/malkki/neostumbler/scanner/autoscan/LocationReceiver.kt
@@ -63,56 +63,55 @@ class LocationReceiver : BroadcastReceiver() {
 
     @SuppressLint("MissingPermission")
     private fun tryStartAutoscan(context: Context, currentLocation: Location) {
-        Timber.i("Removing geofences because new location was received")
+        val reportDao = (context.applicationContext as StumblerApplication).reportDb.reportDao()
 
+        val reportMinTimestamp = Instant.now().minus(Duration.ofDays(30))
+        Timber.d("Querying reports newer than $reportMinTimestamp")
+        val reports = runBlocking { reportDao.getReportsNewerThan(reportMinTimestamp) }
+
+        val reportsNearCurrentLocation = reports.count { report ->
+            Location("manual").apply {
+                latitude = report.latitude
+                longitude = report.longitude
+            }.distanceTo(currentLocation) <= REPORT_RADIUS
+        }
+
+        val canStartScanningHere = reportsNearCurrentLocation < MAX_REPORTS_AUTOSTART
+
+        Timber.i("Current location is ${currentLocation.latitude}, ${currentLocation.longitude}. $reportsNearCurrentLocation reports have been made near (< ${REPORT_RADIUS}m) the current location within last month. Start autoscanning: $canStartScanningHere")
+
+        //NOTE: Can't use coroutine API .await() with geofences here because it causes geofence requests to hang for some reason
         val geofencingClient = LocationServices.getGeofencingClient(context)
 
-        //Can't use coroutine API .await() here because it causes geofence removal request to hang for some reason
-        geofencingClient.removeGeofences(listOf(AUTOSCAN_GEOFENCE_REQUEST_ID))
-            .continueWith {
-                Timber.i("Geofences removed")
+        //If less than 5 reports have been created near the current location in the last month, start scanning
+        if (canStartScanningHere) {
+            context.startForegroundService(ScannerService.startIntent(context, autostart = true))
 
-                val reportDao = (context.applicationContext as StumblerApplication).reportDb.reportDao()
+            //Remove geofences because scanning was started and we don't need them anymore
+            geofencingClient.removeGeofences(listOf(AUTOSCAN_GEOFENCE_REQUEST_ID))
+                .addOnSuccessListener { Timber.i("Removed geofences") }
+                .addOnFailureListener { e -> Timber.w(e, "Failed to remove geofences") }
+        } else {
+            //Otherwise create geofence to detect when user has moved away from the current location and try again
+            val geofencingRequest = GeofencingRequest.Builder()
+                .addGeofence(Geofence.Builder()
+                    .setRequestId(AUTOSCAN_GEOFENCE_REQUEST_ID)
+                    //Use slightly larger radius for geofence to make it more likely that scanning is possible when user exits geofence
+                    .setCircularRegion(currentLocation.latitude, currentLocation.longitude, REPORT_RADIUS.toFloat() * 1.2f)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .setNotificationResponsiveness(60 * 1000)
+                    .setExpirationDuration(30 * 60 * 1000)
+                    .build()
+                )
+                .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_EXIT)
+                .build()
 
-                val reportMinTimestamp = Instant.now().minus(Duration.ofDays(30))
-                Timber.d("Querying reports newer than $reportMinTimestamp")
-                val reports = runBlocking { reportDao.getReportsNewerThan(reportMinTimestamp) }
+            Timber.i("Adding geofence to detect when user moves away from the current location")
 
-                val reportsNearCurrentLocation = reports.count { report ->
-                    Location("manual").apply {
-                        latitude = report.latitude
-                        longitude = report.longitude
-                    }.distanceTo(currentLocation) <= REPORT_RADIUS
-                }
-
-                val canStartScanningHere = reportsNearCurrentLocation < MAX_REPORTS_AUTOSTART
-
-                Timber.i("Current location is ${currentLocation.latitude}, ${currentLocation.longitude}. $reportsNearCurrentLocation reports have been made near (< ${REPORT_RADIUS}m) the current location within last month. Start autoscanning: $canStartScanningHere")
-
-                //If less than 5 reports have been created near the current location in the last month, start scanning
-                if (canStartScanningHere) {
-                    context.startForegroundService(ScannerService.startIntent(context, autostart = true))
-                } else {
-                    //Otherwise create geofence to detect when user has moved away from the current location and try again
-                    val geofencingRequest = GeofencingRequest.Builder()
-                        .addGeofence(Geofence.Builder()
-                            .setRequestId(AUTOSCAN_GEOFENCE_REQUEST_ID)
-                            .setCircularRegion(currentLocation.latitude, currentLocation.longitude, REPORT_RADIUS.toFloat() * 1.2f) //Use slightly larger radius for geofence to make it more likely that scanning is possible when user exits geofence
-                            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_EXIT)
-                            .setNotificationResponsiveness(60 * 1000)
-                            .setExpirationDuration(30 * 60 * 1000)
-                            .build()
-                        )
-                        .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_EXIT)
-                        .build()
-
-                    Timber.i("Adding geofence to detect when user moves away from the current location")
-
-                    geofencingClient.addGeofences(geofencingRequest, getPendingIntent(context))
-                        .addOnSuccessListener { Timber.i("Added geofences") }
-                        .addOnFailureListener { e -> Timber.w(e, "Failed to add geofences") }
-                }
-            }
+            geofencingClient.addGeofences(geofencingRequest, getPendingIntent(context))
+                .addOnSuccessListener { Timber.i("Added geofences") }
+                .addOnFailureListener { e -> Timber.w(e, "Failed to add geofences") }
+        }
     }
 
     private fun handleGeofencingEvent(context: Context, geofencingEvent: GeofencingEvent) {
