75eba5352e3d4194e29ee0f4f0c0a26c2b76e17f
Jaakko Malkki <jaakko.malkki234@gmail.com>
2024-05-21 21:05:53 +0300

    Disable geocoding after 3 IOExceptions

--- app/src/main/java/xyz/malkki/neostumbler/utils/geocoder/PlatformGeocoder.kt
+++ app/src/main/java/xyz/malkki/neostumbler/utils/geocoder/PlatformGeocoder.kt
@@ -4,15 +4,34 @@ import android.location.Address
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withPermit
 import xyz.malkki.neostumbler.extensions.getFromLocationSuspending
+import java.io.IOException
+import java.util.concurrent.atomic.AtomicInteger
 import android.location.Geocoder as AndroidGeocoder
 
 //Limit the amount of concurrent requests to avoid UI lag
 private const val MAX_CONCURRENT_REQUESTS = 5
 
-class PlatformGeocoder(private val geocoder: AndroidGeocoder, private val maxResults: Int = 1): Geocoder {
+private const val MAX_FAILURES = 3
+
+class PlatformGeocoder(private val geocoder: AndroidGeocoder, private val maxResults: Int = 1, private val returnEmptyIfNoGeocoder: Boolean = true): Geocoder {
     private val semaphore = Semaphore(MAX_CONCURRENT_REQUESTS)
 
-    override suspend fun getAddresses(latitude: Double, longitude: Double): List<Address> = semaphore.withPermit {
-        return geocoder.getFromLocationSuspending(latitude, longitude, maxResults)
+    //Count the number of IOExceptions to stop using the geocoder
+    private val failureCount = AtomicInteger(0)
+
+    override suspend fun getAddresses(latitude: Double, longitude: Double): List<Address> {
+        if ((returnEmptyIfNoGeocoder && !AndroidGeocoder.isPresent()) || failureCount.get() >= MAX_FAILURES) {
+            return emptyList()
+        }
+
+        return try {
+            semaphore.withPermit {
+                geocoder.getFromLocationSuspending(latitude, longitude, maxResults)
+            }
+        } catch (e: IOException) {
+            failureCount.incrementAndGet()
+
+            throw e
+        }
     }
 }
