175b9bdb4d4331e703a8baa6860d94f71e9b18ab
Jaakko Malkki <jaakko.malkki234@gmail.com>
2024-10-04 21:01:33 +0300

    Simplify active mobile subscription listener flow by using a handler thread

--- app/src/main/java/xyz/malkki/neostumbler/extensions/subscriptionManagerExtensions.kt
+++ app/src/main/java/xyz/malkki/neostumbler/extensions/subscriptionManagerExtensions.kt
@@ -3,7 +3,7 @@ package xyz.malkki.neostumbler.extensions
 import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Build
-import android.os.Looper
+import android.os.HandlerThread
 import android.telephony.SubscriptionManager
 import android.telephony.SubscriptionManager.OnSubscriptionsChangedListener
 import androidx.annotation.RequiresPermission
@@ -12,48 +12,39 @@ import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.yield
 import xyz.malkki.neostumbler.utils.ImmediateExecutor
-import kotlin.concurrent.thread
 
 @RequiresPermission(Manifest.permission.READ_PHONE_STATE)
 fun SubscriptionManager.getActiveSubscriptionIds(): Flow<List<Int>> = callbackFlow {
-    var looper: Looper? = null
     var listener: OnSubscriptionsChangedListener? = null
 
-    //Separate thread has to be used here because OnSubscriptionsChangedListener needs a thread with a Looper
-    thread {
-        Looper.prepare()
+    val handlerThread = object : HandlerThread("ActiveSubscriptionHandler") {
+            override fun onLooperPrepared() {
+                listener = object : OnSubscriptionsChangedListener() {
+                    @SuppressLint("MissingPermission")
+                    override fun onSubscriptionsChanged() {
+                        val subscriptions = this@getActiveSubscriptionIds.activeSubscriptionInfoList ?: emptyList()
 
-        looper = Looper.myLooper()!!
+                        trySendBlocking(subscriptions.map { it.subscriptionId })
+                    }
+                }
 
-        listener = object : OnSubscriptionsChangedListener() {
-            @SuppressLint("MissingPermission")
-            override fun onSubscriptionsChanged() {
-                val subscriptions = this@getActiveSubscriptionIds.activeSubscriptionInfoList ?: emptyList()
-
-                trySendBlocking(subscriptions.map { it.subscriptionId })
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    this@getActiveSubscriptionIds.addOnSubscriptionsChangedListener(ImmediateExecutor, listener)
+                } else {
+                    @Suppress("DEPRECATION")
+                    this@getActiveSubscriptionIds.addOnSubscriptionsChangedListener(listener)
+                }
             }
         }
-
-        Looper.loop()
-    }
-
-    //Busywait because it shouldn't take long to prepare the looper
-    while (listener == null) {
-        yield()
-    }
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        this@getActiveSubscriptionIds.addOnSubscriptionsChangedListener(ImmediateExecutor, listener)
-    } else {
-        @Suppress("DEPRECATION")
-        this@getActiveSubscriptionIds.addOnSubscriptionsChangedListener(listener)
-    }
+        .apply {
+            start()
+        }
 
     awaitClose {
         this@getActiveSubscriptionIds.removeOnSubscriptionsChangedListener(listener)
-        looper!!.quit()
+
+        handlerThread.quit()
     }
 }.distinctUntilChanged { old, new ->
     //Only emit new values if the IDs have changed
