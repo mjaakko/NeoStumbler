5f4a85ccc8df5b7735f8bf39536129d950070997
Jaakko Malkki <jaakko.malkki234@gmail.com>
2024-10-15 19:14:14 +0300

    Use a splash screen to avoid flickering colors when the dynamic color theme is used

--- app/src/main/java/xyz/malkki/neostumbler/ui/theme/Theme.kt
+++ app/src/main/java/xyz/malkki/neostumbler/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package xyz.malkki.neostumbler.ui.theme
 
 import android.app.Activity
-import android.content.Context
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -11,17 +10,11 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import xyz.malkki.neostumbler.StumblerApplication
-import xyz.malkki.neostumbler.constants.PreferenceKeys
 
 private val DarkColorScheme = darkColorScheme(
     primary = Color(0xffffb4a8),
@@ -48,41 +41,33 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
-private fun Context.useDynamicColor(): Flow<Boolean> = (applicationContext as StumblerApplication).settingsStore.data
-    .map { prefs ->
-        prefs[booleanPreferencesKey(PreferenceKeys.DYNAMIC_COLOR_THEME)] == true
-    }
-
 @Composable
 fun NeoStumblerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean,
     content: @Composable () -> Unit
 ) {
-    val dynamicColorState = LocalContext.current.useDynamicColor().collectAsState(initial = null)
-
-    dynamicColorState.value?.let { dynamicColor ->
-        val colorScheme = when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
-
-            darkTheme -> DarkColorScheme
-            else -> LightColorScheme
-        }
-        val view = LocalView.current
-        if (!view.isInEditMode) {
-            SideEffect {
-                val window = (view.context as Activity).window
-                window.statusBarColor = colorScheme.primary.toArgb()
-                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-            }
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
-        )
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
 }
