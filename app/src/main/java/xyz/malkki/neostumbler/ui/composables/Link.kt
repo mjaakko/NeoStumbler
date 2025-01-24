a06565b2af5c4c82a803651ae2c856d75c9f1ffe
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-01-24 21:00:34 +0200

    Replace ClickableText with a custom composable Link

--- app/src/main/java/xyz/malkki/neostumbler/ui/composables/Link.kt
+++ app/src/main/java/xyz/malkki/neostumbler/ui/composables/Link.kt
@@ -2,23 +2,46 @@ package xyz.malkki.neostumbler.ui.composables
 
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withLink
+
+@Composable
+fun Link(text: String, onClick: () -> Unit, style: TextStyle = MaterialTheme.typography.bodySmall) {
+    Text(
+        text = buildAnnotatedString {
+            withLink(
+                link = LinkAnnotation.Clickable(
+                    tag = "link",
+                    styles = TextLinkStyles(style.copy(color = MaterialTheme.colorScheme.primary).toSpanStyle()),
+                    linkInteractionListener = {
+                        onClick.invoke()
+                    }
+                )
+            ) {
+                append(text)
+            }
+        },
+        style = style
+    )
+}
 
 @Composable
 fun Link(text: String, url: String, style: TextStyle = MaterialTheme.typography.bodySmall) {
     val context = LocalContext.current
 
-    ClickableText(
-        text = AnnotatedString(text),
-        style = style.copy(color = MaterialTheme.colorScheme.primary),
+    Link(
+        text = text,
         onClick = {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             context.startActivity(intent)
-        }
+        },
+        style = style
     )
 }
