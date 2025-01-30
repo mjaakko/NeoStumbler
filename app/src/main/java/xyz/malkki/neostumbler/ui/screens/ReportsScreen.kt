e4b16a66ec11cc1cb11bb475821719d7cd94bc9f
Thore Goebel <goebel.thore@gmail.com>
2025-01-30 13:34:59 +0100

    Add play/stop icon to 'Start scanning' button

--- app/src/main/java/xyz/malkki/neostumbler/ui/screens/ReportsScreen.kt
+++ app/src/main/java/xyz/malkki/neostumbler/ui/screens/ReportsScreen.kt
@@ -15,13 +15,18 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,6 +38,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -272,11 +278,25 @@ fun ForegroundScanningButton() {
             startScanning()
         }
     ) {
-        val stringResId = if (serviceConnection.value != null) {
+        val isScanning = serviceConnection.value != null
+
+        val stringResId = if (isScanning) {
             R.string.stop_scanning
         } else {
             R.string.start_scanning
         }
+        val icon = if (isScanning) {
+            Icons.Default.Stop
+        } else {
+            Icons.Default.PlayArrow
+        }
+
+        Icon(
+            painter = rememberVectorPainter(icon),
+            contentDescription = null,
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
 
         Text(text = stringResource(stringResId))
     }
