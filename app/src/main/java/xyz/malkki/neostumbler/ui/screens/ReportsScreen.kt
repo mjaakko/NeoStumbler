e2d18b706d2136b11e456bfff75a318d4a38580d
Thore Goebel <goebel.thore@gmail.com>
2025-01-30 23:09:04 +0100

    Ensure icon height is same as text height

--- app/src/main/java/xyz/malkki/neostumbler/ui/screens/ReportsScreen.kt
+++ app/src/main/java/xyz/malkki/neostumbler/ui/screens/ReportsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -33,13 +34,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -458,17 +462,29 @@ private fun Report(report: ReportWithStats, geocoder: Geocoder, onDeleteReport: 
 private fun StationCount(iconRes: Int, iconDescription: String, count: Int) {
     val decimalFormat = remember { DecimalFormat("0") }
 
+    val localDensity = LocalDensity.current
+    var textHeightDp by remember { mutableStateOf(0.dp) }
+
     Row(
         modifier = Modifier.wrapContentSize(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        Icon(painter = painterResource(iconRes), contentDescription = iconDescription)
+        Icon(
+            painter = painterResource(iconRes),
+            contentDescription = iconDescription,
+            modifier = Modifier.requiredSize(textHeightDp)
+        )
         Spacer(modifier = Modifier.width(2.dp))
         Text(
             modifier = Modifier
                 .wrapContentWidth()
-                .fillMaxHeight(),
+                .fillMaxHeight()
+                .onGloballyPositioned {
+                    textHeightDp = with(localDensity) {
+                        it.size.height.toDp()
+                    }
+                },
             text = decimalFormat.format(count),
             style = MaterialTheme.typography.bodySmall,
         )
