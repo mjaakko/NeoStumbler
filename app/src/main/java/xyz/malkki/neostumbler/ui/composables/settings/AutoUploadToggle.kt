7dfbc97e415e5447b900919a015c86d1d9bf07c1
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-03-24 21:39:49 +0200

    Add option to select the required network type for automatic report uploads

--- app/src/main/java/xyz/malkki/neostumbler/ui/composables/settings/AutoUploadToggle.kt
+++ app/src/main/java/xyz/malkki/neostumbler/ui/composables/settings/AutoUploadToggle.kt
@@ -1,29 +1,37 @@
 package xyz.malkki.neostumbler.ui.composables.settings
 
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
-import androidx.work.await
+import kotlin.enums.enumEntries
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.toJavaDuration
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import xyz.malkki.neostumbler.R
 import xyz.malkki.neostumbler.ichnaea.ReportSendWorker
-import xyz.malkki.neostumbler.ui.composables.ToggleWithAction
 
 private val UPLOAD_INTERVAL = 8.hours
 
-private fun WorkManager.autoUploadEnabled(): Flow<Boolean> =
+private enum class AutoUploadMode(@StringRes val description: Int) {
+    NEVER(R.string.send_reports_automatically_never),
+    ANY_NETWORK(R.string.send_reports_automatically_on_any_network),
+    UNMETERED_NETWORK(R.string.send_reports_automatically_on_unmetered_network),
+}
+
+private fun WorkManager.getAutoUploadWorkInfo(): Flow<WorkInfo?> =
     getWorkInfosForUniqueWorkFlow(ReportSendWorker.PERIODIC_WORK_NAME).map { workInfos ->
-        workInfos.any { workInfo ->
+        workInfos.find { workInfo ->
             workInfo.state != WorkInfo.State.CANCELLED && workInfo.state != WorkInfo.State.FAILED
         }
     }
@@ -34,36 +42,50 @@ fun AutoUploadToggle() {
 
     val workManager = WorkManager.getInstance(context)
 
-    val autoUploadEnabled = workManager.autoUploadEnabled().collectAsState(initial = null)
+    val autoUploadWorkInfo by workManager.getAutoUploadWorkInfo().collectAsStateWithLifecycle(null)
 
-    ToggleWithAction(
-        title = stringResource(R.string.send_reports_automatically),
-        enabled = autoUploadEnabled.value != null,
-        checked = autoUploadEnabled.value == true,
-        action = { checked ->
-            if (checked) {
-                // Schedule report uploading to MLS
-                workManager
-                    .enqueueUniquePeriodicWork(
-                        ReportSendWorker.PERIODIC_WORK_NAME,
-                        ExistingPeriodicWorkPolicy.UPDATE,
-                        PeriodicWorkRequestBuilder<ReportSendWorker>(
-                                UPLOAD_INTERVAL.toJavaDuration()
-                            )
-                            .setConstraints(
-                                Constraints(
-                                    requiredNetworkType = NetworkType.CONNECTED,
-                                    requiresCharging = false,
-                                    requiresStorageNotLow = false,
-                                    requiresDeviceIdle = true,
-                                    requiresBatteryNotLow = true,
-                                )
-                            )
-                            .build(),
-                    )
-                    .await()
+    MultiChoiceSettings(
+        title = stringResource(R.string.send_reports_automatically_title),
+        options = enumEntries<AutoUploadMode>(),
+        selectedOption =
+            if (autoUploadWorkInfo == null) {
+                AutoUploadMode.NEVER
+            } else if (
+                autoUploadWorkInfo!!.constraints.requiredNetworkType == NetworkType.UNMETERED
+            ) {
+                AutoUploadMode.UNMETERED_NETWORK
             } else {
-                workManager.cancelUniqueWork(ReportSendWorker.PERIODIC_WORK_NAME).await()
+                AutoUploadMode.ANY_NETWORK
+            },
+        titleProvider = { ContextCompat.getString(context, it.description) },
+        onValueSelected = { autoUploadMode ->
+            if (autoUploadMode == AutoUploadMode.NEVER) {
+                workManager.cancelUniqueWork(ReportSendWorker.PERIODIC_WORK_NAME)
+            } else {
+                val workRequest =
+                    PeriodicWorkRequestBuilder<ReportSendWorker>(UPLOAD_INTERVAL.toJavaDuration())
+                        .setConstraints(
+                            Constraints(
+                                requiredNetworkType =
+                                    if (autoUploadMode == AutoUploadMode.UNMETERED_NETWORK) {
+                                        NetworkType.UNMETERED
+                                    } else {
+                                        NetworkType.CONNECTED
+                                    },
+                                requiresCharging = false,
+                                requiresStorageNotLow = false,
+                                requiresDeviceIdle = true,
+                                requiresBatteryNotLow = true,
+                            )
+                        )
+                        .build()
+
+                // Schedule automatic report uploading to the configured endpoint
+                workManager.enqueueUniquePeriodicWork(
+                    ReportSendWorker.PERIODIC_WORK_NAME,
+                    ExistingPeriodicWorkPolicy.UPDATE,
+                    workRequest,
+                )
             }
         },
     )
