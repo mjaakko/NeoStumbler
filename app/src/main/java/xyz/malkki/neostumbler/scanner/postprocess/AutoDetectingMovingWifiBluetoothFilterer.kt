e6788545ab2a742ff7ba281e55d041b27b51e8e9
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-05-11 19:18:29 +0300

    Add option to automatically filter moving Wi-Fis and Bluetooths

---
+++ app/src/main/java/xyz/malkki/neostumbler/scanner/postprocess/AutoDetectingMovingWifiBluetoothFilterer.kt
@@ -0,0 +1,48 @@
+package xyz.malkki.neostumbler.scanner.postprocess
+
+import xyz.malkki.neostumbler.domain.LatLng
+import xyz.malkki.neostumbler.scanner.data.ReportData
+
+private const val MAX_DISTANCE_FROM_EXISTING = 500.0
+
+class AutoDetectingMovingWifiBluetoothFilterer(
+    private val maxDistanceFromExisting: Double = MAX_DISTANCE_FROM_EXISTING
+) : ReportPostProcessor {
+    private val wifiLocations = mutableMapOf<String, LatLng>()
+    private val bluetoothLocations = mutableMapOf<String, LatLng>()
+
+    private val movingWifis = mutableSetOf<String>()
+    private val movingBluetooths = mutableSetOf<String>()
+
+    override fun postProcessReport(reportData: ReportData): ReportData? {
+        movingWifis +=
+            reportData.wifiAccessPoints
+                .filter { wifi ->
+                    val existingLocation =
+                        wifiLocations.getOrPut(wifi.macAddress) { reportData.position.latLng }
+
+                    existingLocation.distanceTo(reportData.position.latLng) >=
+                        maxDistanceFromExisting
+                }
+                .map { it.macAddress }
+
+        movingBluetooths +=
+            reportData.bluetoothBeacons
+                .filter { bluetooth ->
+                    val existingLocation =
+                        bluetoothLocations.getOrPut(bluetooth.macAddress) {
+                            reportData.position.latLng
+                        }
+
+                    existingLocation.distanceTo(reportData.position.latLng) >=
+                        maxDistanceFromExisting
+                }
+                .map { it.macAddress }
+
+        return reportData.copy(
+            wifiAccessPoints = reportData.wifiAccessPoints.filter { it.macAddress !in movingWifis },
+            bluetoothBeacons =
+                reportData.bluetoothBeacons.filter { it.macAddress !in movingBluetooths },
+        )
+    }
+}
