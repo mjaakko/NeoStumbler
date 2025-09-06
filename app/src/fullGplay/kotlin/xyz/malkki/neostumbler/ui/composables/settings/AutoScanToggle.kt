212d5bcb70fe7381481f0d744d8014861e04f5a9
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-09-06 17:44:19 +0300

    Remove autoscanning feature from the gplay variant

---
+++ app/src/fullGplay/kotlin/xyz/malkki/neostumbler/ui/composables/settings/AutoScanToggle.kt
@@ -0,0 +1,9 @@
+package xyz.malkki.neostumbler.ui.composables.settings
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AutoScanToggle() {
+    // Autoscan not available in the gplay variant to avoid classifying NeoStumbler as a health
+    // application
+}
