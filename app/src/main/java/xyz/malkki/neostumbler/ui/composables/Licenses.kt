14b85d5430140ad32bf61a0cebf811b197807c33
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-09-07 22:42:58 +0300

    Open license links with custom tabs

--- app/src/main/java/xyz/malkki/neostumbler/ui/composables/Licenses.kt
+++ app/src/main/java/xyz/malkki/neostumbler/ui/composables/Licenses.kt
@@ -8,20 +8,33 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
 import com.mikepenz.aboutlibraries.ui.compose.android.rememberLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import xyz.malkki.neostumbler.R
 import xyz.malkki.neostumbler.ui.composables.shared.Dialog
+import xyz.malkki.neostumbler.utils.openUrl
 
 @Composable
 private fun LicensesDialog(onClose: () -> Unit) {
+    val context = LocalContext.current
+
     val libraries by rememberLibraries(R.raw.aboutlibraries)
 
     Dialog(onDismissRequest = onClose, title = stringResource(R.string.third_party_licenses)) {
         LibrariesContainer(
             libraries = libraries,
+            onLibraryClick = { library ->
+                val licenseUrl = library.licenses.firstNotNullOfOrNull { it.url }
+
+                if (licenseUrl != null) {
+                    context.startActivity(openUrl(licenseUrl))
+                } else if (library.website != null) {
+                    context.startActivity(openUrl(library.website!!))
+                }
+            },
             textStyles =
                 LibraryDefaults.libraryTextStyles(
                     nameTextStyle = MaterialTheme.typography.titleMedium,
