c4e6b41b710bc5fbfd38369546e7a078656cd493
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-09-29 22:28:24 +0300

    Add site base URL to Astro config

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://neostumbler.malkki.xyz",
   vite: {
     plugins: [tailwindcss()]
   },
