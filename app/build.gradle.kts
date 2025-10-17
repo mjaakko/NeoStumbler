038071ce3e4ba9183c66df7e055e1a63a0f3d362
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-10-17 23:15:49 +0300

    Update version to 2.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
 
     defaultConfig {
         applicationId = "xyz.malkki.neostumbler"
-        versionCode = 47
-        versionName = "2.1.9-rc3"
+        versionCode = 48
+        versionName = "2.1.9"
 
         androidResources {
             // Configure supported languages here to avoid including incomplete translations in the
