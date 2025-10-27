9b90eed5571e0b4f83206990b1f15ba4f1a36fae
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-10-27 20:02:57 +0200

    Update Ktfmt Gradle plugin to 0.25.0

--- libs/ichnaea/src/test/kotlin/xyz/malkki/neostumbler/ichnaea/IchnaeaClientTest.kt
+++ libs/ichnaea/src/test/kotlin/xyz/malkki/neostumbler/ichnaea/IchnaeaClientTest.kt
@@ -88,15 +88,15 @@ class IchnaeaClientTest {
             MockResponse().apply {
                 setBody(
                     """
-                        {
-                          "license": "test license",
-                          "location": {
-                            "lat": 50.42352,
-                            "lng": 12.32135
-                          },
-                          "accuracy": 25000,
-                          "fallback": "ipf"
-                        }
+                    {
+                      "license": "test license",
+                      "location": {
+                        "lat": 50.42352,
+                        "lng": 12.32135
+                      },
+                      "accuracy": 25000,
+                      "fallback": "ipf"
+                    }
                     """
                         .trimIndent()
                 )
