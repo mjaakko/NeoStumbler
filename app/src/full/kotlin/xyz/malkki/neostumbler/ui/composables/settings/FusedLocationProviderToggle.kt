6722ab568a47d907fb7f4174823648938625168a
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-11-23 19:25:23 +0200

    Don't allow disabling fused location provider when the device does not have a GPS

--- app/src/full/kotlin/xyz/malkki/neostumbler/ui/composables/settings/FusedLocationProviderToggle.kt
+++ app/src/full/kotlin/xyz/malkki/neostumbler/ui/composables/settings/FusedLocationProviderToggle.kt
@@ -3,13 +3,16 @@ package xyz.malkki.neostumbler.ui.composables.settings
 import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import org.koin.compose.koinInject
 import xyz.malkki.neostumbler.R
 import xyz.malkki.neostumbler.constants.PreferenceKeys
+import xyz.malkki.neostumbler.data.location.GpsStatusSource
 import xyz.malkki.neostumbler.data.settings.Settings
 import xyz.malkki.neostumbler.scanner.passive.PassiveScanManager
 import xyz.malkki.neostumbler.ui.composables.ToggleWithAction
@@ -28,15 +31,20 @@ private fun Settings.fusedProviderAndPassiveScanEnabled(): Flow<Pair<Boolean, Bo
 fun FusedLocationProviderToggle(
     settings: Settings = koinInject(),
     passiveScanManager: PassiveScanManager = koinInject(),
+    gpsStatusSource: GpsStatusSource = koinInject(),
 ) {
     val state =
         settings.fusedProviderAndPassiveScanEnabled().collectAsState(initial = true to false)
     val (preferFusedLocationProvider, passiveScanEnabled) = state.value
 
+    val gpsAvailable by
+        gpsStatusSource.isGpsAvailable().collectAsStateWithLifecycle(initialValue = false)
+
     ToggleWithAction(
         title = stringResource(id = R.string.prefer_fused_location_title),
         description = stringResource(id = R.string.prefer_fused_location_description),
-        enabled = true,
+        warningWhenDisabled = stringResource(id = R.string.fused_location_no_gps_warning),
+        enabled = gpsAvailable,
         checked = preferFusedLocationProvider,
         action = { checked ->
             settings.edit { setBoolean(PreferenceKeys.PREFER_FUSED_LOCATION, checked) }
