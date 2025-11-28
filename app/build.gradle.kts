dea80de0d7ce284c369076da9278f8083a640a66
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-11-28 16:23:35 +0200

    Update version to 2.2.0-rc2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
 
     defaultConfig {
         applicationId = "xyz.malkki.neostumbler"
-        versionCode = 50
-        versionName = "2.2.0-rc1"
+        versionCode = 51
+        versionName = "2.2.0-rc2"
 
         androidResources {
             // Configure supported languages here to avoid including incomplete translations in the
