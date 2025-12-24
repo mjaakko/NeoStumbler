c1415a089ed5f13cc7df91cb8665602659df70c0
Jaakko Malkki <jaakko.malkki234@gmail.com>
2025-12-24 16:14:27 +0200

    Delete unused BLE beacon parser

--- libs/beacon-library-utils/src/main/kotlin/xyz/malkki/neostumbler/beaconlibrary/RuuviTagV5BeaconParser.kt
+++
@@ -1,9 +0,0 @@
-package xyz.malkki.neostumbler.beaconlibrary
-
-import org.altbeacon.beacon.BeaconParser
-
-object RuuviTagV5BeaconParser : BeaconParser() {
-    init {
-        setBeaconLayout("m:0-2=990405,i:20-25")
-    }
-}
