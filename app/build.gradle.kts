97b5f49828cb5efa3f9f90f8a9a41786fc27cf46
Jaakko Malkki <jaakko.malkki234@gmail.com>
2026-01-05 21:42:30 +0200

    Update version to 2.3.0-beta2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
 
     defaultConfig {
         applicationId = "xyz.malkki.neostumbler"
-        versionCode = 54
-        versionName = "2.3.0-beta1"
+        versionCode = 55
+        versionName = "2.3.0-beta2"
 
         androidResources {
             // Configure supported languages here to avoid including incomplete translations in the
