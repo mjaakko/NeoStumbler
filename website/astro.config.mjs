f0ffbd3deffa96288b33eb4ada8e8c40b5768903
Jaakko Malkki <jaakko.malkki234@gmail.com>
2026-03-30 20:08:17 +0300

    Enable prefetch for website links

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: "https://neostumbler.malkki.xyz",
+  prefetch: {
+    prefetchAll: true
+  },
   vite: {
     plugins: [tailwindcss()]
   },
