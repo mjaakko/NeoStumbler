23abd559d5c308e176e4994c73dc91c487858ee6
Jaakko Malkki <jaakko.malkki234@gmail.com>
2026-07-02 20:38:13 +0300

    Update version to 2.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
 
     defaultConfig {
         applicationId = "xyz.malkki.neostumbler"
-        versionCode = 63
-        versionName = "2.3.5"
+        versionCode = 64
+        versionName = "2.3.6"
 
         androidResources {
             // Configure supported languages here to avoid including incomplete translations in the
